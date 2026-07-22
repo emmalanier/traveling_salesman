1ec194661a141c4a4a21a2132eba216c0d262a36
Emma Lanier <emma.lanier@outlook.fr>
2026-07-22 18:20:18 +0200

    Create header.h
    header

---
+++ header.h
@@ -0,0 +1,13 @@
+//HEADER FILE//
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <fstream>
+#include <vector>
+
+#endif //HEADER_H
