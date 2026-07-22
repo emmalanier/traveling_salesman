3b920f2e3a236e320af7a521b77504cd4133c717
Emma Lanier <emma.lanier@outlook.fr>
2026-07-22 18:52:37 +0200

    Create methods.cpp
    getters

---
+++ methods.cpp
@@ -0,0 +1,23 @@
+//METHODS.CPP//
+
+#include "header.h"
+
+double destination::get_lat()
+{
+  return m_lat;
+}
+
+double destination::get_long()
+{
+  return m_long;
+}
+
+double destination::get_alt()
+{
+  return m_alt;
+}
+
+std::string destination::get_name()
+{
+  return m_name;
+}
