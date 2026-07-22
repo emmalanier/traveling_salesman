cb0d86a48607598efc57e7b614a298ea154613d2
Emma Lanier <emma.lanier@outlook.fr>
2026-07-22 18:31:58 +0200

    Update header.h
    Coded class destination

--- header.h
+++ header.h
@@ -10,4 +10,24 @@
 #include <fstream>
 #include <vector>
 
+class destination
+{
+  private :
+    double m_lat;
+    double m_long;
+    double m_alt;
+    std::string m_name;
+
+  public :
+    double get_lat();
+    double get_long();
+    double get_alt();
+    std::string get_name();
+
+    void set_lat(const double&);
+    void set_long(const double&);
+    void set_alt(const double&);
+    void set_name(const std::string&);
+};
+
 #endif //HEADER_H
