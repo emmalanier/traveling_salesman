95da23532d72f1a1ea6d7e3575bef0279c1ac550
emmalanier <emma.lanier@outlook.fr>
2026-08-06 16:03:10 +0200

    Beginning of implementation of formatting methods

---
+++ include/shapes.h
@@ -0,0 +1,21 @@
+//SHAPES COORDINATES GENERATION//
+
+#ifndef SHAPES_H
+#define SHAPES_H
+
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
+struct vertex
+{
+    float x, y, z, nx, ny, nz, r, g, b ;
+};
+
+std::vector<float> format_sphere_file(std::vector<std::string>&);
+std::vector<vertex> compute_sphere_vertices(std::vector<float>&);
+
+#endif//SHAPES_H
---
+++ src/rendering_shapes.cpp
@@ -0,0 +1,78 @@
+//SHAPES//
+
+#include "header.h"
+#include "shapes.h"
+
+std::vector<float> format_sphere_file(std::vector<std::string>& vec)
+{
+   std::vector<float> results;
+   float x;
+   float y;
+   float z;
+
+   std::string x_str;
+   std::string y_str;
+   std::string z_str;
+
+   bool got_x;
+   bool got_y;
+
+   for(int i=0; vec.size(); i++)
+   {
+     if(i!=0)
+     {
+        x = stof(x_str);
+        y = stof(y_str);
+        z = stof(z_str);
+     }
+
+     got_x = false;
+     got_y = false;
+
+        for(int j=0; j<vec[i].length(); j++)
+        {
+            if(vec[i][j]=='\n' && !got_x)
+            {
+                got_x = true;
+            }
+            else if(vec[i][j]=='\n' && got_x)
+            {
+                got_y = true;
+            }
+
+            else if(!got_x)
+            {
+                x_str+=vec[i][j];
+            }
+
+            else if(!got_y)
+            {
+                y_str+=vec[i][j];
+            }
+
+            else
+            {
+                z_str+=vec[i][j];
+            }
+        }
+   }
+   
+   return results ;
+}
+
+std::vector<vertex> compute_sphere_vertices(std::vector<float>& vec)
+{
+    std::vector<vertex> results;
+    vertex point;
+
+    float x_var;
+    float y_var;
+    float z_var;
+
+    for(int i=0; i<vec.size(); i++)
+    {
+
+    }
+
+    return results;
+}
